b7b58c14c03e8fb6c30cccc642c5f87ac1353da6
wu1274704958 <1274704958@qq.com>
2019-08-23 19:12:46 +0800

    add __fields get_field_name

--- main.cpp
+++ main.cpp
@@ -51,10 +51,48 @@ public:
 	std::string acc;
 	std::string psd;
 	std::string head;
+
+	constexpr static auto __fields = std::make_tuple(
+		&user::id,"id",
+		&user::name,"name",
+		&user::sex,"sex",
+		&user::age,"age",
+		&user::acc,"acc",
+		&user::psd,"psd",
+		&user::head,"head"
+	);
+	
+	template<size_t I, typename CLS, typename F>
+	constexpr static const char * get_field_name_sub(F CLS::* f) {
+
+		if constexpr (std::is_same_v<std::remove_const_t<std::remove_reference_t<decltype(std::get<I>(__fields))>>, F CLS::*>)
+		{
+			if (std::get<I>(__fields) == f)
+			{
+				return std::get<I + 1>(__fields);
+			}
+		}
+		if constexpr((I + 2) < std::tuple_size<decltype(__fields)>::value)
+		{
+			return get_field_name_sub<I + 2, CLS, F>(f);
+		}
+		return nullptr;
+	}
+
+	template<typename CLS,typename F>
+	constexpr static const char * get_field_name(F CLS::* f) {
+		return get_field_name_sub<0,CLS,F>(f);
+	}
 };
 
 int main()
 {
+
+
+	cout << user::get_field_name(&user::id) << endl;
+	cout << user::get_field_name(&user::name) << endl;
+	cout << user::get_field_name(&user::acc) << endl;
+	cout << user::get_field_name(&user::head) << endl;
 	try {
 
 		Drive dri;
